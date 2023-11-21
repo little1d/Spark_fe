6a24ce784111f4b08358b8fd13a47698acb3e99f
xyr <1747298135@qq.com>
2023-11-21 20:04:06 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
     你好
   </div>
 
+
   <div class="grid grid-cols-3 gap-4 mt-8">
-      <div class="bg-blue-200 p-4">Item 1</div>
+      <div class="bg-blue-200 p-4">hello</div>
       <div class="bg-green-200 p-4">Item 2</div>
       <div class="bg-yellow-200 p-4 font-bold">Item 3</div>
     </div>
